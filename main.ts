c1f8c64e1b4b4e7b5a66637012b588fe8e165afb
MartinHoly00 <115696258+MartinHoly00@users.noreply.github.com>
2022-12-15 11:35:05 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,88 @@
+let stranakostky = 0
+let hodit = false
+let tip = 1
+basic.showNumber(tip)
+input.onButtonPressed(Button.A, function () {
+    tip -= 1
+    if (tip == 0) {
+        tip = 1
+        basic.showNumber(tip)
+    } else
+        basic.showNumber(tip)
+})
+
+input.onButtonPressed(Button.B, function () {
+    tip += 1
+    if (tip == 7) {
+        tip = 6
+        basic.showNumber(tip)
+    } else
+        basic.showNumber(tip)
+})
+
+input.onGesture(Gesture.Shake, function () {
+    stranakostky = randint(1, 6)
+    hodit = true
+    if (stranakostky == 1) {
+        basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `
+        )
+    } else if (stranakostky == 2) {
+        basic.showLeds(`
+    . . . . .
+    . . . # .
+    . . . . .
+    . # . . .
+    . . . . .
+    `
+        )
+    } else if (stranakostky == 3) {
+        basic.showLeds(`
+    . . . . .
+    . . . # .
+    . . # . .
+    . # . . .
+    . . . . .
+    `
+        )
+    } else if (stranakostky == 4) {
+        basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    . # . # .
+    . . . . .
+    `
+        )
+    } else if (stranakostky == 5) {
+        basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . # . .
+    . # . # .
+    . . . . .
+    `
+        )
+    } else if (stranakostky == 6) {
+        basic.showLeds(`
+    . . . . .
+    . # . # .
+    . # . # .
+    . # . # .
+    . . . . .
+    `
+        )
+    }
+
+    if (stranakostky == tip && hodit) {
+        music.playTone(131, music.beat(BeatFraction.Half))
+    } else {
+        music.playTone(131, music.beat(BeatFraction.Half))
+    }
+})
+
